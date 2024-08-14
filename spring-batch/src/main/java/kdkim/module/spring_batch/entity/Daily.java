8efb2af296e24dc7480d4f7c1945f80a07e011ee
apffjfjd <115337453+apffjfjd@users.noreply.github.com>
2024-08-14 18:02:41 +0900

    commit

--- spring-batch/src/main/java/kdkim/module/spring_batch/entity/Daily.java
+++ spring-batch/src/main/java/kdkim/module/spring_batch/entity/Daily.java
@@ -5,11 +5,17 @@ import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import kdkim.module.spring_batch.entity.composite_key.DailyCompositeKey;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "daily")
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Daily {
 
     @EmbeddedId
--- spring-batch/src/main/java/kdkim/module/spring_batch/repository/DailyRepository.java
+++ spring-batch/src/main/java/kdkim/module/spring_batch/repository/DailyRepository.java
@@ -2,6 +2,8 @@ package kdkim.module.spring_batch.repository;
 
 import kdkim.module.spring_batch.entity.Daily;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DailyRepository extends JpaRepository<Daily, Long> {
 }
